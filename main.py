01e5e52646552d84b8d63af8c65d23a7f01484ca
Nguyen Khanh Toan <126448566+ktoan911@users.noreply.github.com>
2024-04-18 10:11:10 +0700

    Update param model

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def generate_response(prompt):
 
 parser = ArgumentParser()
 # Định nghĩa model muốn sử dụng
-parser.add_argument("--model", default="liemsbot1", type=str)
+parser.add_argument("--model", required=True, type=str)
 args = parser.parse_args()
 model = args.model
 
